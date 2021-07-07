38fc93c96f54f6d4ef4665dfcf307639ec8dbcb8
Daniel Sitarek <deltasierra777@gmail.com>
2021-07-07 18:11:43 -0500

    cleaned up code

--- index.js
+++ index.js
@@ -1,54 +1,53 @@
-let metric = document.getElementById("metric");
+let metricConversionCheckbox = document.getElementById("metric");
+let ListofLengths = "";
+let materialSum = undefined;
+let previousMaterialSum = 0;
 
-const matsBlock = () => {
-  let mats = document.getElementById("matsBlock").value;
-  return mats.split(" ").map(Number);
+const materialInput = () => {
+  let material = document.getElementById("materialInput").value;
+  return material.split(" ").map(Number);
 };
 
-const clearMats = () => {
+const clearMaterial = () => {
   document.getElementById("matList").innerHTML = "";
   document.getElementById("total").innerHTML = "";
-  //matsSum = 0;
+};
+
+const calculateMaterialTotal = () => {
+  materialSum = 0;
+  let arrayOfLengths = materialInput();
+  for (i = 0; i < arrayOfLengths.length; i++) {
+    materialSum += arrayOfLengths[i];
+  }
+  if (metricConversionCheckbox.checked) {
+    materialSum = materialSum / 25.4;
+  }
+  materialSum = materialSum + previousMaterialSum;
+  previousMaterialSum = materialSum;
+  materialSum = Number(materialSum.toFixed(3));
+  return materialSum;
 };
 
 showSum = () => {
   let totalLength = document.getElementById("total");
-  totalLength.innerHTML = `<br>Total length: ${matsSum}</br>`;
+  totalLength.innerHTML = `<br>Total length: ${materialSum}</br>`;
   document.getElementById("inputForm").appendChild(totalLength);
 };
 
-let splitListRef = "";
-let matsSum = 0;
-let prevSum = 0;
-
-const calcMat = () => {
-  matsSum = 0;
-  let arrayOfLengths = matsBlock();
-  for (i = 0; i < arrayOfLengths.length; i++) {
-    matsSum += arrayOfLengths[i];
-  }
-  if (metric.checked) {
-    matsSum = matsSum / 25.4;
-  }
-  matsSum = matsSum + prevSum;
-  prevSum = matsSum;
-  return matsSum;
-};
-
 const splitter = () => {
-  if (metric.checked) {
-    matsBlock().forEach(function (item) {
-      splitListRef += "<li class='list'>" + (item / 25.4).toFixed(3) + "</li>";
+  if (metricConversionCheckbox.checked) {
+    materialInput().forEach(function (item) {
+      ListofLengths += "<li class='list'>" + (item / 25.4).toFixed(3) + "</li>";
     });
   } else {
-    matsBlock().forEach(function (item) {
-      splitListRef += "<li class='list'>" + Number(item).toFixed(3) + "</li>";
+    materialInput().forEach(function (item) {
+      ListofLengths += "<li class='list'>" + Number(item).toFixed(3) + "</li>";
     });
   }
-  document.getElementById("matList").innerHTML = splitListRef;
-  calcMat();
+  document.getElementById("matList").innerHTML = ListofLengths;
+  calculateMaterialTotal();
   showSum();
 };
 
 document.getElementById("submitBtn").addEventListener("click", splitter);
-document.getElementById("clearBtn").addEventListener("click", clearMats);
+document.getElementById("clearBtn").addEventListener("click", clearMaterial);
